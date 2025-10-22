dbd9975eba6ed376a12624957006082de18b9d31
Nick Coleman <nicholasfcoleman@gmail.com>
2025-10-22 16:15:25 -0600

    Initial Commit

---
+++ BBQuotes/BBQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotesApp.swift
+//  BBQuotes
+//
+//  Created by Nick Coleman on 10/22/25.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
