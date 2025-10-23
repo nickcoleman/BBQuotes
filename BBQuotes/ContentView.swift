ec85ce6c03f9f96d38d77a3ec55efdf3fabff8f6
Nick Coleman <nicholasfcoleman@gmail.com>
2025-10-23 09:11:19 -0600

    initial setup

--- BBQuotes/ContentView.swift
+++ BBQuotes/ContentView.swift
@@ -9,13 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            Tab("Breaking Bad", systemImage: "tortoise") {
+                Text("Breaking Bad View")
+            }
+            Tab("Better Call Saul", systemImage: "briefcase") {
+                Text("Better Call Saul View")
+            }
         }
-        .padding()
+        .preferredColorScheme(.dark)
+        .tint(.orange)
     }
 }
 
